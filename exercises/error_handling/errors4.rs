f8fee238b1a546367b186241555903f3916193b9
Tom-CaoZH <tomcaoTTT@outlook.com>
2022-10-22 11:39:21 +0000

    finish quiz3 and before

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,20 +1,30 @@
 // errors4.rs
 // Make this test pass! Execute `rustlings hint errors4` for hints :)
 
-// I AM NOT DONE
+//
+
+use std::sync::mpsc::Receiver;
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
 #[derive(PartialEq, Debug)]
-enum CreationError {
+pub enum CreationError {
     Negative,
     Zero,
 }
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value > 0 {
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
+        else if value ==0 {
+            Err(CreationError::Zero)
+        }
+        else {
+            Err(CreationError::Negative)
+        }
     }
 }
 
